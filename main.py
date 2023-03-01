91cd11a38132d084b7443e32cb566d24fdfdc588
Alberto <elmisterroboto@gmail.com>
2023-03-01 17:39:46 -0600

    Se agregaron docstrings

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Esta es la función de ataque.
+    Recibe nombre y clase del personaje
+    y devuelve un string con la cantidad
+    de daño impartido.
+    """
     if char_class == 'guerrero':
-        return (f'{char_name} causó '
-                f'{5 + randint(3, 5)} de daño al enemigo')
+        return (f'{char_name} causó {5 + randint(3, 5)} de daño al enemigo')
     if char_class == 'mago':
-        return (f'{char_name} causó '
-                f'{5 + randint(5, 10)} de daño al enemigo')
+        return (f'{char_name} causó {5 + randint(5, 10)} de daño al enemigo')
     if char_class == 'sanador':
-        return (f'{char_name} causó '
-                f'{5 + randint(-3, -1)} de daño al enemigo')
+        return (f'{char_name} causó {5 + randint(-3, -1)} de daño al enemigo')
 
 
 def defense(char_name: str, char_class: str) -> str:
+    """Esta es la función de defensa.
+    Recibe nombre y clase del personaje
+    y devuelve un string con la cantidad
+    de daño bloqueado.
+    """
     if char_class == 'guerrero':
         return (f'{char_name} bloqueó {10 + randint(5, 10)} de daño')
     if char_class == 'mago':
@@ -23,72 +30,74 @@ def defense(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Esta es la función de habilidades especiales.
+    Recibe nombre y clase del personaje
+    y devuelve un string con incrementos
+    en atributos seleccionados.
+    """
     if char_class == 'guerrero':
-        return (f'{char_name} usó una habilidad especial '
-                f'"Aguante {80 + 25}"')
+        return f'{char_name} usó una habilidad especial "Aguante {80 + 25}"'
     if char_class == 'mago':
-        return (f'{char_name} usó una habilidad especial "Ataque  {5 + 40}»')
+        return f'{char_name} usó una habilidad especial "Ataque {5 + 40}"'
     if char_class == 'sanador':
-        return (f'{char_name} usó una habilidad especial "Defensa {10 + 30}»')
+        return f'{char_name} usó una habilidad especial "Defensa {10 + 30}"'
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Esta es la función de entrenamiento.
+    Recibe nombre y clase del personaje,
+    devuelve un string con un mensaje personalizado,
+    en función de la clase del personaje
+    y ofrece la posibilidad de entrenar.
+    en atributos seleccionados.
+    """
     if char_class == 'guerrero':
-        print(f"""{char_name}, eres un Guerrero, experto en
-              combate cuerpo a cuerpo.""")
-    if char_class == 'mago':
-        print(f"""{char_name}, eres un Mago, dominando
-              magistralmente los elementos.""")
-    if char_class == 'sanador':
+        print(f'{char_name}, eres un Guerrero, experto '
+              'en combate cuerpo a cuerpo.')
+    elif char_class == 'mago':
+        print(f'{char_name}, eres un Mago, dominando '
+              'magistralmente los elementos.')
+    elif char_class == 'sanador':
         print(f'{char_name}, eres un Sanador, un hechicero que cura heridas.')
     print('Entrena tus habilidades.')
-    print('Introduce uno de los comandos: atacar, para atacar el enemigo, '
-          'defender: para bloquear el ataque enemigo, o '
-          'especial: para usar tu poder especial.')
+    print('Introduce uno de estos comandos: atacar, para atacar un enemigo; '
+          'defender, para bloquear un ataque enemigo; o '
+          'especial, para usar tu poder especial.')
     print('Si no quieres entrenar, presiona saltar (skip).')
     cmd: str = None
     while cmd != 'skip':
         cmd = input('Introduce un comando: ')
-        if cmd == 'atacar':
+        if cmd == 'ataque':
             print(attack(char_name, char_class))
-        if cmd == 'defender':
+        elif cmd == 'defensa':
             print(defense(char_name, char_class))
-        if cmd == 'especial':
+        elif cmd == 'especial':
             print(special(char_name, char_class))
     return 'El entrenamiento ha terminado.'
 
 
 def choice_char_class() -> str:
+    """Permite elegir la clase con la que
+    jugará el personaje.
+    """
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
         char_class = input('Introduce la clase '
-                           'de tu personaje: Guerrero, guerrero; '
+                           'de tu personajes: Guerrero, guerrero; '
                            'Mago, mago; Sanador, sanador: ')
-        if char_class == 'guerrero':
+        if char_class == 'warrior':
             print('Guerrero: un feroz luchador cuerpo a cuerpo. '
                   'Fuerte, resiliente y valiente.')
-        if char_class == 'mago':
+        if char_class == 'magician':
             print('Mago: un brillante luchador de largo alcance. '
                   'Maestro de poderes mágicos.')
-        if char_class == 'sanador':
+        if char_class == 'healer':
             print('Sanador: un poderoso chamán. '
                   'Extrae fuerza de la naturaleza, la fe y los espíritus.')
         approve_choice = input('Presiona (Y) para confirmar '
                                'o cualquier otro botón '
-                               'para seleccionar otra clase').lower()
+                               'para seleccionar cualquier otra clase').lower()
     return char_class
 
-
-def main():
-    print('¡Saludos, aventurero!')
-    print('Antes de comenzar a jugar...')
-    char_name: str = input('... indica tu nombre: ')
-    print(f'¡Bienvenido, {char_name}! '
-          """Tienes 80 puntos de aguante, 5 puntos
-          de ataque, y 10 puntos de defensa.""")
-    print('Puedes elegir una de las tres maneras de la Fuerza:')
-    print('Guerrero, Mago, Sanador')
-    char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
     
